cc188d8670d11e59b5e1362bee47dd1747a79d1e
saniil <anilsever.ce@gmail.com>
2023-07-18 16:32:36 +0300

    header da bir sürü styled componet özelliği kullandık

--- components/Header.js
+++ components/Header.js
@@ -5,11 +5,29 @@ const HeaderDiv = styled.div`
   background-color: tomato;
   padding: 20px;
   border: 10px solid blue;
+  display: flex;
+  gap: 20px;
+  span {
+    color: gray;
+    font-size: 25px;
+  }
+`;
+const SpanDiv = styled.div`
+  color: green;
+  &:hover {
+    text-decoration: underline;
+  }
+  font-size: ${({ big }) => (big ? "40px" : "20px")};
+  border: ${({ borderSize }) => `${borderSize}px solid blue`};
 `;
 
 export default () => (
   <HeaderDiv>
     <Link href={"/"}>Anasayfa</Link>
     <Link href={"/about"}>About</Link>
+    <SpanDiv big borderSize={5}>
+      EK açıklama
+    </SpanDiv>
+    <span>Normal bir span</span>
   </HeaderDiv>
 );
