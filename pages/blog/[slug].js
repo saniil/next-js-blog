2df7c00925ceecd3404b881e5924f261789c1f65
saniil <anilsever.ce@gmail.com>
2023-07-19 14:18:45 +0300

    md dosyalarıyla çalışır hale getirildi

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -2,48 +2,87 @@ import Layout from "../../components/Layout";
 import { useRouter } from "next/router";
 import Error from "next/error";
 
-const data = [
-  {
-    yazi: "gunluk-1",
-    title: "Depreme Dayanıklı evler",
-    text: `Lorem Ipsum is simply dummy text of the printing and typesetting
-  industry. Lorem Ipsum has been the industry's standard dummy text ever
-  since the 1500s, when an unknown printer took a galley of type and
-  scrambled it to make a type specimen book. It has survived not only five
-  centuries, but also the leap into electronic typesetting, remaining
-  essentially unchanged. It was popularised in the 1960s with the release
-  of Letraset sheets containing Lorem Ipsum passages, and more recently
-  with desktop publishing software like Aldus PageMaker including versions
-  of Lorem Ipsum`,
-  },
-  {
-    yazi: "gunluk-2",
-    title: "Elektrikte Yeni Devrimler",
-    text: `o electronic typesetting, remaining
-essentially unchanged. It was popularised in the 1960s with the release
-of Letraset sknown printer took a galley of type and
-scrambled it to make a type specimen book. It has survived not only five
-centuries, but also the leap intheets containing Lorem Ipsum passages, and more recently
-with desktop publishing software like Aldus PageMaker including versions
-of Lorem IpsumLorem Ipsum is simply dummy text of the printing and typesetting
-industry. Lorem Ipsum has been the industry's standard dummy text ever
-since the 1500s, when an un`,
-  },
-];
+import fs from "fs";
+import matter from "gray-matter";
+import md from "markdown-it";
 
-export default ({}) => {
-  const router = useRouter();
-  const { slug } = router.query;
-  let foundItem = data.find((dataItem) => dataItem.yazi === slug);
-  console.log(foundItem);
+// const data = [
+//   {
+//     yazi: "gunluk-1",
+//     title: "Depreme Dayanıklı evler",
+//     text: `Lorem Ipsum is simply dummy text of the printing and typesetting
+//   industry. Lorem Ipsum has been the industry's standard dummy text ever
+//   since the 1500s, when an unknown printer took a galley of type and
+//   scrambled it to make a type specimen book. It has survived not only five
+//   centuries, but also the leap into electronic typesetting, remaining
+//   essentially unchanged. It was popularised in the 1960s with the release
+//   of Letraset sheets containing Lorem Ipsum passages, and more recently
+//   with desktop publishing software like Aldus PageMaker including versions
+//   of Lorem Ipsum`,
+//   },
+//   {
+//     yazi: "gunluk-2",
+//     title: "Elektrikte Yeni Devrimler",
+//     text: `o electronic typesetting, remaining
+// essentially unchanged. It was popularised in the 1960s with the release
+// of Letraset sknown printer took a galley of type and
+// scrambled it to make a type specimen book. It has survived not only five
+// centuries, but also the leap intheets containing Lorem Ipsum passages, and more recently
+// with desktop publishing software like Aldus PageMaker including versions
+// of Lorem IpsumLorem Ipsum is simply dummy text of the printing and typesetting
+// industry. Lorem Ipsum has been the industry's standard dummy text ever
+// since the 1500s, when an un`,
+//   },
+// ];
 
-  if (!foundItem) {
-    return <Error statusCode={404} />;
-  }
+export default ({ frontmatter, content }) => {
+  const { title, author, category, date, bannerImage, tags } = frontmatter;
+
+  // const router = useRouter();
+  // const { slug } = router.query;
+  // let foundItem = data.find((dataItem) => dataItem.yazi === slug);
+  // console.log(foundItem);
+
+  // if (!foundItem) {
+  //   return <Error statusCode={404} />;
+  // }
   return (
-    <Layout title={foundItem?.title}>
-      <h1>{slug}:burası blog/... ne geliyorsa ona eşittir.</h1>
-      <p>{foundItem?.text}</p>
+    <Layout title={title}>
+      <h2>
+        {author} || {date}
+      </h2>
+      <h3>
+        {category} || {tags.join()}
+      </h3>
+      <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
     </Layout>
   );
 };
+
+export async function getStaticPaths() {
+  // Get list of all files from our posts directory
+  const files = fs.readdirSync("posts");
+  // Generate a path for each one
+  const paths = files.map((fileName) => ({
+    params: {
+      slug: fileName.replace(".md", ""),
+    },
+  }));
+  // return list of paths
+  return {
+    paths,
+    fallback: false,
+  };
+}
+
+// Generate the static props for the page
+export async function getStaticProps({ params: { slug } }) {
+  const fileName = fs.readFileSync(`posts/${slug}.md`, "utf-8");
+  const { data: frontmatter, content } = matter(fileName);
+  return {
+    props: {
+      frontmatter,
+      content,
+    },
+  };
+}
