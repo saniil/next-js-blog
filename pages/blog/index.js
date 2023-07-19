ec29f2de69424d28947fa01a089c253132f9e71e
saniil <anilsever.ce@gmail.com>
2023-07-19 14:33:23 +0300

    md dosyalarıylaçlışır hale getirildi

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,6 +1,7 @@
 import Layout from "../../components/Layout";
 import { styled } from "styled-components";
 import Button from "../../components/Button";
+import getStaticPaths from "./[slug]";
 
 const BlogDiv = styled.div`
   background-color: rgba(0, 0, 0, 0.2);
@@ -10,7 +11,7 @@ const BlogDiv = styled.div`
 
 const blogContents = [
   {
-    href: "gunluk-1",
+    href: "konu1",
     title: "Günlük yazı 1",
     text: "here are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.",
   },
